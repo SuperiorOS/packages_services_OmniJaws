38a5bca933a0c300cb60ab91c27f869637855d28
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-16 06:36:55 +0800

    OmniJaws: Memory leaks solution
    Change-Id: If8977698f05824b3ba245a165d86ab9a50791781
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
+++ app/src/main/java/org/omnirom/omnijaws/SettingsFragment.java
@@ -77,7 +77,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     private Handler mHandler = new Handler();
     protected boolean mShowIconPack = true;
     private EditTextPreference mOwmKey;
-    private OmniJawsClient mWeatherClient;
     private Preference mCustomLocationActivity;
     private static final String PREF_KEY_CUSTOM_LOCATION = "weather_custom_location";
     private static final String WEATHER_ICON_PACK = "weather_icon_pack";
@@ -116,7 +115,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         mPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
-        mWeatherClient = new OmniJawsClient(getContext());
 
         doLoadPreferences();
     }
@@ -192,7 +190,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     @Override
     public void onResume() {
         super.onResume();
-        mWeatherClient.addObserver(this);
+        OmniJawsClient.get().addObserver(getContext(), this);
         // values can be changed from outside
         getPreferenceScreen().removeAll();
         doLoadPreferences();
@@ -206,7 +204,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     @Override
     public void onPause() {
         super.onPause();
-        mWeatherClient.removeObserver(this);
+        OmniJawsClient.get().removeObserver(getContext(), this);
     }
 
     @Override
@@ -411,13 +409,13 @@ public class SettingsFragment extends PreferenceFragmentCompat implements OnPref
     }
 
     private void queryAndUpdateWeather() {
-        mWeatherClient.queryWeather();
-        if (mWeatherClient.getWeatherInfo() != null) {
+        OmniJawsClient.get().queryWeather(getContext());
+        if (OmniJawsClient.get().getWeatherInfo() != null) {
             getActivity().runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     if (mUpdateStatus != null) {
-                        mUpdateStatus.setSummary(mWeatherClient.getWeatherInfo().getLastUpdateTime());
+                        mUpdateStatus.setSummary(OmniJawsClient.get().getWeatherInfo().getLastUpdateTime());
                     }
                 }
             });
